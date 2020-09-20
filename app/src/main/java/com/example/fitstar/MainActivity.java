7eae820a3658d3bf2bdb5f938479d8fcf96b3f48
mukulchhabra24 <mchha001@plattsburgh.edu>
2020-09-19 23:08:44 -0400

    9/19

--- app/src/main/java/com/example/fitstar/MainActivity.java
+++ app/src/main/java/com/example/fitstar/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.fitstar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// ...
+// Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
     }
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
+
 }
